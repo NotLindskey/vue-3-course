f1d7ebfea97c69366d8cd5f9016f666e22dddde8
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-06 20:49:21 -0500

    added v-html text to post.body

--- linssi-colin-ruiz-portfolio/script.js
+++ linssi-colin-ruiz-portfolio/script.js
@@ -2,11 +2,11 @@ const posts = [
   {
     id: 1,
     title: "Why I learned Vue",
-    body: `I'm baby chambray street art thundercats occupy four loko
+    body: `I'm baby chambray street art <strong>thundercats</strong> occupy four loko
                 church-key disrupt. Shaman neutra bushwick chicharrones, tousled
                 air plant lomo williamsburg. Listicle aesthetic whatever prism,
                 ennui glossier asymmetrical scenester austin intelligentsia
-                cronut raw denim umami mumblecore. Lo-fi meh austin, selfies
+                cronut raw denim umami mumblecore. <br>Lo-fi meh austin,</br> selfies
                 hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore
                 tumeric."`,
   },
