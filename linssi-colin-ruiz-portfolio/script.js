5b8a1b0b8a3e6191195e3e96897aef3b42c562ad
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-06 19:59:27 -0500

    binding attributes

--- linssi-colin-ruiz-portfolio/script.js
+++ linssi-colin-ruiz-portfolio/script.js
@@ -1,52 +1,53 @@
 const posts = [
-        {
-          id: 1,
-          title: "Why I learned Vue",
-          body: `I'm baby chambray street art thundercats occupy four loko
+  {
+    id: 1,
+    title: "Why I learned Vue",
+    body: `I'm baby chambray street art thundercats occupy four loko
                 church-key disrupt. Shaman neutra bushwick chicharrones, tousled
                 air plant lomo williamsburg. Listicle aesthetic whatever prism,
                 ennui glossier asymmetrical scenester austin intelligentsia
                 cronut raw denim umami mumblecore. Lo-fi meh austin, selfies
                 hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore
                 tumeric."`,
-        },
-        {
-          id: 2,
-          title: "Using the Vue CDN",
-          body: `I'm baby chambray street art thundercats occupy four loko
+  },
+  {
+    id: 2,
+    title: "Using the Vue CDN",
+    body: `I'm baby chambray street art thundercats occupy four loko
                 church-key disrupt. Shaman neutra bushwick chicharrones, tousled
                 air plant lomo williamsburg. Listicle aesthetic whatever prism,
                 ennui glossier asymmetrical scenester austin intelligentsia
                 cronut raw denim umami mumblecore. Lo-fi meh austin, selfies
                 hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore
                 tumeric."`,
-        },
-        {
-          id: 3,
-          title: "I am using the vue CDN vue",
-          body: `I'm baby chambray street art thundercats occupy four loko
+  },
+  {
+    id: 3,
+    title: "I am using the vue CDN vue",
+    body: `I'm baby chambray street art thundercats occupy four loko
                 church-key disrupt. Shaman neutra bushwick chicharrones, tousled
                 air plant lomo williamsburg. Listicle aesthetic whatever prism,
                 ennui glossier asymmetrical scenester austin intelligentsia
                 cronut raw denim umami mumblecore. Lo-fi meh austin, selfies
                 hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore
                 tumeric. tacos are great!"`,
-        },
-      ]
+  },
+];
 
 Vue.createApp({
   data() {
     return {
       name: "Linssi Colin Ruiz",
       links: ["home", "portfolio", "contact me"],
-    };
-  },
-}).mount("header");
-
-Vue.createApp({
-  data() {
-    return {
       posts,
+      darkModeSet: true,
+      darkMode: {
+        background: "#38383a",
+        color: "whitesmoke",
+      },
+      base: {
+        fontFamily: "monospace"
+      }
     };
   },
-}).mount("#blog");
+}).mount("body");
