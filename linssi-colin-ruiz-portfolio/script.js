4e4483570fa22b566639ed004afce4c2568e2be6
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-08 17:12:27 -0500

    code clean up

--- linssi-colin-ruiz-portfolio/script.js
+++ linssi-colin-ruiz-portfolio/script.js
@@ -87,11 +87,11 @@ const app = Vue.createApp({
     toggleMode() {
       this.darkModeSet = !this.darkModeSet;
     },
-    async getPosts() {
-      // let response = await fetch("https://jsonplaceholder.typicode.com/posts");
-      // let data = await response.json();
-      // console.log(data);
-    },
+    // async getPosts() {
+    //   let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    //   let data = await response.json();
+    //   console.log(data);
+    // },
   },
 });
 
